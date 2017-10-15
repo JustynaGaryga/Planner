115a8655fc2e7e688a0480b6aa53da3fa4f91366
JustynaGaryga <justyna.garyga@gmail.com>
2017-10-15 21:49:20 +0200

    DEV added corrected task 5

--- Week1task5/src/Array.java
+++ Week1task5/src/Array.java
@@ -3,22 +3,20 @@ public class Array {
 
 	public static void main(String[] args) {
 		//initialising an array
-			int[] number = {2, 20, 16, 8, 10, 6, 4, 14, 12, 18};
-			printArray(number);
+		int[] number = {2, 20, 16, 8, 10, 6, 4, 14, 12, 18};
+		printArray(number);
 
 	}
 	public static void printArray(int[] number) {
 		// printing array in a single line
-		System.out.print(number[0]);
-		for (int i = 1; i < number.length; i++) {
+		for (int i = 0; i < number.length; i++) {
 			System.out.print(" " + number[i]);
 		}
 		System.out.println();
 		
 		// printing sorted array in a single line
 		Arrays.sort(number);
-		System.out.print(number[0]);
-		for (int i = 1; i < number.length; i++) {
+		for (int i = 0; i < number.length; i++) {
 			System.out.print(" " + number[i]);
 		}
 		System.out.println();
