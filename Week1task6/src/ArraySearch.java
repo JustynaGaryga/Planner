f4486ca841bf5543c020433d511c5051b24c284c
JustynaGaryga <justyna.garyga@gmail.com>
2017-10-18 11:01:45 +0200

    DEV added correct task 6

--- Week1task6/src/ArraySearch.java
+++ Week1task6/src/ArraySearch.java
@@ -15,13 +15,24 @@ public class ArraySearch {
 		int foundValue = Arrays.binarySearch(number, searchValue);
 		System.out.println("Searching existing value: " + searchValue);
 		System.out.println("Found value: " + number[foundValue]);
+		System.out.println("Index of value " + number[foundValue] + " : " + foundValue);
 		
 		// searching an unexisting value in the array
 		int searchValue2 = 2;
 		int foundValue2 = Arrays.binarySearch(number, searchValue2);
 		System.out.println("Searching unexisting value: " + searchValue2);
-		System.out.println("Found value: " + number[foundValue2]);
-
+		System.out.println(foundValue2);
+		if (foundValue2 != -1 && foundValue2 < number.length) {
+			System.out.println(number[foundValue2]);
+		}
+			else {
+				System.out.println("The value " + searchValue2 + " does not exist in the array");
+				}
 	}
+}	
+		
+	
+	
+	
+	
 
-}
