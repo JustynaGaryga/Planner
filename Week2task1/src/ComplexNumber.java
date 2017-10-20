cb020fd4199616b0fac82582a6a054c408b9a744
JustynaGaryga <justyna.garyga@gmail.com>
2017-10-20 20:51:23 +0200

    DEV added Week2task 1

--- Week2task1/src/ComplexNumber.java
+++ Week2task1/src/ComplexNumber.java
@@ -47,21 +47,20 @@ public class ComplexNumber {
 		return product;
 	}
 	// printing the complex number in the format: a + bi (real + "+" + im + "*" + "i")
-	public String toString(ComplexNumber num1, ComplexNumber num2) {
-		String format = (real + "+" + im + "*" + "i");  
+	public String toString() {
+		String format = (this.real + "+" + this.im + "*" + "i");  
 		return format;
 	}
 	// testing if two complex numbers are equal
-	public static void equals(ComplexNumber num1, ComplexNumber num2) {
+	public static boolean equals(ComplexNumber num1, ComplexNumber num2) {
 		if (num1.real == num2.real) {
 			if (num1.im == num2.im) {
-			System.out.println("The complex numbers are equals.");
+				return true;
 			}
-		}else {
-		System.out.println("The complex numbers are not equals.");
 		}
+		return false;
 	}
-	
+		
 	public static void main(String[] ags) {
 		// TODO Auto-generated method stub
 		//my examples of complex numbers
@@ -70,10 +69,10 @@ public class ComplexNumber {
 		
 		System.out.println("Complex number 1: " + "(" + num1.real + ", " + num1.im + ")");
 		System.out.println("Complex number 2: " + "(" + num2.real + ", " + num2.im + ")");
-		System.out.println("Sum of complex numbers: " + sum(num1, num2));
-		System.out.println("Product of complex numbers: " + product(num1, num2));
-		System.out.println("Complex number 1= " + num1.toString(num1, num2));
-		System.out.println("Complex number 2= " + num2.toString(num1, num2));
-		equals(num1, num2);
+		System.out.println("Sum of complex numbers: " + sum(num1, num2).toString());
+		System.out.println("Product of complex numbers: " + product(num1, num2).toString());
+		System.out.println("Complex number 1= " + num1.toString());
+		System.out.println("Complex number 2= " + num2.toString());
+		System.out.println(equals(num1, num2));
 	}
 }
