6fb96883fdce7ce1afe62425654e245b2f9e7411
JustynaGaryga <justyna.garyga@gmail.com>
2017-10-25 12:23:51 +0200

    DEV added task 2 with changes

--- Week2task2/src/Main.java
+++ Week2task2/src/Main.java
@@ -14,21 +14,16 @@ public class Main {
 		Name person1 = new Name("Justyna", "Garyga"); 
 		Name person2 = new Name("Anna", "Smith"); 
 		
-		// create ArrayList, sort and print sorted list
+		// creates ArrayList, sort and print sorted list
 		List<Name> people = new ArrayList<>();
 		people.add(person);
 		people.add(person1);
 		people.add(person2);
-	
 		Collections.sort(people);
-		//Arrays.sort and Collections.sort don't work
-	
+		
+		// calls toString() and equals methods
 		System.out.println("List of people: " + people.toString());
 		System.out.println("Comparison of two Name objects: " + person1.equals(person2));
 		person1.compareTo(person2);
-		
-		
-		
-	
 	}
 }
