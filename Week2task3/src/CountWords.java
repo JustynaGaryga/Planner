0f53202c5de7f4c07fdd1b4fe2720b8008659c30
JustynaGaryga <justyna.garyga@gmail.com>
2017-10-25 12:07:15 +0200

    DEV corrected task 3 (Week 2) and the program correctly counts words in the text

--- Week2task3/src/CountWords.java
+++ Week2task3/src/CountWords.java
@@ -11,21 +11,21 @@ public class CountWords {
 		// TODO Auto-generated method stub
 		String lonleyDay = "Such a lonely day and it's mine, the most loneliest day of my life. Such a lonely day.";
 		HashMap<String, Integer> hm = new HashMap();
-		String[] words = lonleyDay.split(" ,.?!'");
+		String[] words = lonleyDay.split("[ ,.?!/]");
 		for (int i = 0; i < words.length; i++) {
 			System.out.println(words[i]);
 		}
-		ArrayList<String> words = {"Such", "a", "lonley", "day", "and", "it", "s", "day", "the", "most"};
+		
 		for (int j = 0; j < words.length; j++) {
-			word = words.get(j);
+			String word = words[j];
 			if (hm.containsKey(word)) {
 				hm.put(word, hm.get(word)+1);
 				// first time encountering a word
 			} else {
 				hm.put(word, 1);
 			}
-
 		}
+		System.out.println(hm);
 	}
 
 }
