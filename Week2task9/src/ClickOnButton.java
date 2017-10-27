d507234003ed36708d106cb1ddadac709ce1f6a2
JustynaGaryga <justyna.garyga@gmail.com>
2017-10-27 10:41:46 +0200

    DEV added task 9 (Week2)- Java Swing

---
+++ Week2task9/src/ClickOnButton.java
@@ -0,0 +1,31 @@
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class ClickOnButton extends JFrame  implements ActionListener {
+	
+	public ClickOnButton() {
+		super( "Learn IT Girl" );
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(200, 200);
+		setLocation(400, 200);
+		setLayout(new FlowLayout(70, 50, 50));
+		JButton b = new JButton("Click me!");
+		b.addActionListener(this);
+		add(b);
+		setVisible(true);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		// show a dialog with the message
+		int numClicks = 0;
+		numClicks++;
+		Frame frame = null;
+		JOptionPane.showMessageDialog(frame, "You have clicked the button " + numClicks + " times.");
+	}
+}
