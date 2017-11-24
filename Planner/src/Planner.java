7663b746d767148d619db2e10189b2b2a6c7022d
JustynaGaryga <justyna.garyga@gmail.com>
2017-11-24 12:03:38 +0100

    DEV created the calendar in the JTable

--- Planner/src/Planner.java
+++ Planner/src/Planner.java
@@ -3,6 +3,9 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
@@ -20,6 +23,7 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
@@ -27,14 +31,18 @@ import javax.swing.table.DefaultTableModel;
 public class Planner extends JFrame {
 	JFrame plannerFrame = new JFrame("Planner. Learn IT, Girl!");
 	DefaultTableModel model;
+	Calendar cal = new GregorianCalendar(Locale.ENGLISH);
 	JPanel listPanel = new JPanel();
 	JPanel topPanel = new JPanel();
 	JPanel buttonPanel = new JPanel();
+	JPanel changeMonthPanel = new JPanel();
+	JPanel calPanel = new JPanel();
 	JButton addUserButton = new JButton("Add new User");
 	JButton addTaskButton = new JButton("Add new Task");
 	JButton selectTaskButton = new JButton("Choose new task");
 	JLabel userLabel = new JLabel (" USERS: ", SwingConstants.LEFT);
 	JLabel taskLabel = new JLabel (" TASKS: ", SwingConstants.LEFT);
+	JLabel monthLabel = new JLabel();
 	
 	DefaultComboBoxModel<User> usersList = new DefaultComboBoxModel<>();
 	DefaultComboBoxModel<Task> tasksList = new DefaultComboBoxModel<>();
@@ -44,7 +52,7 @@ public class Planner extends JFrame {
 	public Planner() {
 		super ("Planner. Learn IT, Girl!");
 		plannerFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		plannerFrame.setSize(800, 600);
+		plannerFrame.setSize(800, 500);
 		plannerFrame.setLocation(200, 100);
 		plannerFrame.getContentPane().setBackground(Color.yellow); // temporary color
 		plannerFrame.setVisible(true);
@@ -141,7 +149,7 @@ public class Planner extends JFrame {
 			    
 			    }}});
 				
-		//add buttons to panelButtons
+		// add buttons to panelButtons
 		buttonPanel.add(addUserButton);
 	    buttonPanel.add(addTaskButton);
 	    buttonPanel.setSize(200, 200);
@@ -149,8 +157,8 @@ public class Planner extends JFrame {
 		
 		// User's list
 		User user1 = new User("Justyna", "Garyga");
-		User user2 = new User("Piter", "Garyga");
-		User user3 = new User("Kris", "Garyga");
+		User user2 = new User("Peter", "Garyga");
+		User user3 = new User("Chris", "Garyga");
 		User user4 = new User("Barbara", "Garyga");
 		
 		usersList.addElement(user1);
@@ -175,10 +183,11 @@ public class Planner extends JFrame {
 		tasksList.addElement(task6);
 		tasksList.addElement(task7);
 		
-		Border border = BorderFactory.createLineBorder(Color.BLACK);
+		Border border = BorderFactory.createEtchedBorder(EtchedBorder.RAISED);
 		userLabel.setBorder(border); 
 		taskLabel.setBorder(border);
 		
+		// the panel with list on the left side
 		listPanel.add(userLabel); 
 		listPanel.add(listWithUsers);
 		listPanel.add(Box.createRigidArea(new Dimension(0,20)));
@@ -186,23 +195,24 @@ public class Planner extends JFrame {
 		listPanel.add(listWithTasks);
 		listPanel.setBackground(Color.white);
 		listPanel.setLayout(new BoxLayout(listPanel, BoxLayout.Y_AXIS));
-		listPanel.setAlignmentY(LEFT_ALIGNMENT); // correct alignment to the left (labels!) 
 		
-		// layout
+		// the panel with buttons on the top
 		topPanel.setLayout(new BorderLayout());
 		topPanel.setBackground(Color.white);
 		topPanel.add(buttonPanel, BorderLayout.LINE_END);
 		topPanel.add(selectTaskButton, BorderLayout.LINE_START);
 		plannerFrame.add(topPanel, BorderLayout.PAGE_START);
-		plannerFrame.add(listPanel, BorderLayout.LINE_START);
+		plannerFrame.add(listPanel, BorderLayout.LINE_START); 
 		
-		//show a dialog for edit user, when we click on the user in User's list
+		// show a dialog for edit user, when we click on the user in User's list
 		listWithUsers.addListSelectionListener(new ListSelectionListener() {
 			public void valueChanged(ListSelectionEvent evtU) {
 				JTextField userName = new JTextField(25);
 			    JTextField userSurname = new JTextField(25);
 			    JPanel userPanel = new JPanel();
 			    JButton deleteUser = new JButton("Delete User");
+			    userName.setText(listWithUsers.getSelectedValue().toString());
+			    userSurname.setText(listWithUsers.getSelectedValue().toString());
 			    userPanel.add(new JLabel("Edit name:"));
 			    userPanel.add(userName);
 			    userPanel.add(Box.createVerticalStrut(15)); // a spacer
@@ -213,13 +223,14 @@ public class Planner extends JFrame {
 				if (evtU.getValueIsAdjusting() == false) {
 					int result = JOptionPane.showConfirmDialog(null, userPanel, 
 				               "Edit the User", JOptionPane.OK_CANCEL_OPTION);
-					 if (result == JOptionPane.OK_OPTION) {
+					if (result == JOptionPane.OK_OPTION) {
 				         System.out.println("Name: " + userName.getText());
 				         System.out.println("Surname: " + userSurname.getText());
 				         User editUser = (User)listWithUsers.getSelectedValue();
 				         editUser.setName(userName.getText());
 				         editUser.setSurname(userSurname.getText());
 					 
+				         // button to delete the user from the list of users
 				         deleteUser.addActionListener(new ActionListener() {
 							@Override
 							public void actionPerformed(ActionEvent arg0) {
@@ -231,7 +242,7 @@ public class Planner extends JFrame {
 			}
 		});
 		
-		//show a dialog for edit task, when we click on the task
+		// show a dialog for edit task, when we click on the task
 		listWithTasks.addListSelectionListener(new ListSelectionListener() {
 			public void valueChanged(ListSelectionEvent evtT) {
 				JComboBox selectTask = new JComboBox(tasksList);
@@ -242,6 +253,8 @@ public class Planner extends JFrame {
 			    JTextField taskDescription = new JTextField(30);
 			    JButton deleteTask = new JButton("Delete the task");
 				JPanel taskPanel = new JPanel();
+				taskName.setText(listWithTasks.getSelectedValue().toString());
+			    taskDescription.setText(listWithTasks.getSelectedValue().toString());
 			    taskPanel.add(new JLabel("Edit name of task:"));
 			    taskPanel.add(taskName);
 			    taskPanel.add(Box.createVerticalStrut(15)); // a spacer
@@ -256,41 +269,96 @@ public class Planner extends JFrame {
 			    taskPanel.add(deleteTask);
 			    taskPanel.setLayout(new BoxLayout(taskPanel, BoxLayout.Y_AXIS));
 				if (evtT.getValueIsAdjusting() == false) {
-					 int result = JOptionPane.showConfirmDialog(null, taskPanel, 
+					int result = JOptionPane.showConfirmDialog(null, taskPanel, 
 				               "Edit the task", JOptionPane.OK_CANCEL_OPTION);
-					 if (result == JOptionPane.OK_OPTION) {
-				         Task editTask = (Task)listWithTasks.getSelectedValue();
-				         editTask.setNameTask(taskName.getText());
-				         editTask.setDescriptionTask(taskDescription.getText());
-				         editTask.setStartTime(start.getText());
-				         editTask.setEndTime(end.getText());
-				         System.out.println("Name of task: " + taskName.getText());
-				         System.out.println("Description of task: " + taskDescription.getText());
-				         System.out.println("Start: " + editTask.getStartTime());
-				         System.out.println("End: " + editTask.getEndTime());
-				         System.out.println("Assigned to: " + editTask.getAssignedTo());
-				         
-				         deleteTask.addActionListener(new ActionListener() {
-								@Override
-								public void actionPerformed(ActionEvent arg0) {
-									int i = tasksList.getIndexOf(editTask);
-								    tasksList.removeElementAt(i);
-									}});
+					if (result == JOptionPane.OK_OPTION) {
+						Task editTask = (Task)listWithTasks.getSelectedValue();
+						editTask.setNameTask(taskName.getText());
+						editTask.setDescriptionTask(taskDescription.getText());
+						editTask.setStartTime(start.getText());
+						editTask.setEndTime(end.getText());
+						System.out.println("Name of task: " + taskName.getText());
+						System.out.println("Description of task: " + taskDescription.getText());
+						System.out.println("Start: " + editTask.getStartTime());
+						System.out.println("End: " + editTask.getEndTime());
+						System.out.println("Assigned to: " + editTask.getAssignedTo());
+						
+					 	// button to delete the task from the list of tasks
+					 	deleteTask.addActionListener(new ActionListener() {
+				 			@Override
+				 			public void actionPerformed(ActionEvent arg0) {
+				 				int i = tasksList.getIndexOf(editTask);
+				 				tasksList.removeElementAt(i);
+				 			}});
+					 
 					 }
-				}
 				System.out.println("Selected from " + evtT.getFirstIndex() + " to " + evtT.getLastIndex());
-			}
-		});	
+	
+				}
+		}});	
 		
-		//JTable for calendar
-		String[] columnNames = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+		// JTable for calendar
+		String[] columnNames = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 		
 		model = new DefaultTableModel(null, columnNames);
 		JTable calendar = new JTable(model);
+	    monthLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		calendar.setRowHeight(70);
+		calendar.setCellSelectionEnabled(true); // cells are clicable
+		calendar.setShowGrid(true); 
+		calendar.setGridColor(Color.LIGHT_GRAY);
 		JScrollPane sp=new JScrollPane(calendar);
 		int rowCount = 5;
 		model.setRowCount(rowCount);
-		plannerFrame.add(sp);
-	}      
+		
+		JButton buttonLow = new JButton("<-");
+			buttonLow.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent ae) {
+					cal.add(Calendar.MONTH, -1);
+					updateMonth();
+				}
+		    });
+			
+		JButton buttonHight = new JButton("->");
+			buttonHight.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent ae) {
+					cal.add(Calendar.MONTH, +1);
+					updateMonth();
+				}
+		    });
+		
+		// panel for changing the months and the calendar
+		changeMonthPanel.add(buttonLow, BorderLayout.WEST);
+		changeMonthPanel.add(monthLabel, BorderLayout.NORTH);
+		changeMonthPanel.add(buttonHight, BorderLayout.EAST);
+		changeMonthPanel.setBackground(Color.GREEN);
+		calPanel.add(changeMonthPanel);
+		calPanel.add(sp);
+		calPanel.setLayout(new BoxLayout(calPanel, BoxLayout.Y_AXIS));
+		plannerFrame.add(calPanel);
+		
+		this.updateMonth();
+	}
+	
+	//method to update month 
+	void updateMonth() {
+		cal.set(Calendar.DAY_OF_MONTH, 1);
+		     
+		String month = cal.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.US);
+		int year = cal.get(Calendar.YEAR);
+		monthLabel.setText(month + " " + year);
+		     
+		int startDay = cal.get(Calendar.DAY_OF_WEEK);
+		int numberOfDays = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+		int weeks = cal.getActualMaximum(Calendar.WEEK_OF_MONTH);
+		
+		model.setRowCount(0);
+		model.setRowCount(weeks);
+		
+		int i = startDay-1;
+		for(int day=1;day<=numberOfDays;day++){
+			model.setValueAt(day, i/7 , i%7 );    
+			i = i + 1;
+		}
+	}     
 }
